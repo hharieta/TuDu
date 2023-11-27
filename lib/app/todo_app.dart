889d6408f26713646dd10c91b5d93c84b917950e
Gatovsky <101161255+hharieta@users.noreply.github.com>
2023-11-26 20:15:52 -0500

    first commit

---
+++ lib/app/todo_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tudu/screens/screens.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TuDu: Notas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tudu/app/todo_app.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
